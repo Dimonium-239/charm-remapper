dbb8f1a0ba56ab142fbdcbf34655fddc3db3db9d
dimonium_239 <dimon041220000@gmail.com>
2020-06-09 21:59:32 +0200

    Add json with layouts and its parser

--- remapper.py
+++ remapper.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3.8
 
-import os, signal
-import sys
+import os, sys, signal
 import subprocess 
 import pyautogui
+import json 
 
 class Remapper: 
     def __init__(self):
@@ -15,17 +15,30 @@ class Remapper:
 
     def main(self, argv):
         if len(argv) == 2:
-            if argv[1] == '-h':
-                print(f'{argv[0]} [-u | -r]') 
+            if argv[1] in ('-h', '--help'):
+                print(f'''{argv[0]} [OPTIONS]
+        -h, --help
+            Show short info about options
+        -u, --upper
+            Set selected text to upper case
+        -l, --lower
+            Set selected text to lower case
+        -s, --swapcase
+            Swap case of selected text
+        -r, --remapper
+            Remap chars of selected text following current layout
+                    ''') 
                 sys.exit()
             elif argv[1] in ('-u', '--upper'):
                 self.__sizeChanger(upper=True)
             elif argv[1] in ('-l', '--lower'):
                 self.__sizeChanger(low=True)
-            elif argv[1] in ('-sc', '--swapcase'):
+            elif argv[1] in ('-s', '--swapcase'):
                 self.__sizeChanger(swapcase=True)
             elif argv[1] in ('-r', '--remapper'):
-                print('remapper')
+                self.remapper()
+            else:
+                print(f'Try \'{argv[0]} -h\' for more information.')
 
     def __sizeChanger(self, upper=False, low=False, swapcase=False):
         
@@ -49,6 +62,7 @@ class Remapper:
             ctrlC = subprocess.Popen(['xclip', '-selection', 'c'], stdin=subprocess.PIPE)
             ctrlC.communicate(bytes(var, encoding='utf8'))
             ctrlC.wait()
+
             pyautogui.hotkey('ctrl', 'v')
     
     # TODO: make normal remapper :) 
@@ -66,50 +80,49 @@ class Remapper:
     def remapper(self):
 
         lytBeforProc = subprocess.Popen(['xkblayout-state', 'print', '"%n"'], stdout=subprocess.PIPE) #setxkbmap
-        lytBefor = lytBeforProc.communicate()[0].decode("utf-8") 
+        lytBefor = lytBeforProc.communicate()[0].decode("utf-8")[1:-1]
         lytBeforProc.poll()
-        lytBefor = lytBefor[1:-1]
+        print(lytBefor)
+        with open('layouts.json') as f:
+            data = json.load(f)
+        
+        print(data[lytBefor])
+        #subprocess.Popen(['killall', 'xkblayout-state'])
 
-        changer = subprocess.Popen(['xkblayout-state', 'set', '+1'])
-        changerEx = changer.poll()
+        # lytBefor = lytBefor[1:-1]
 
-        var = ''
+        # changer = subprocess.Popen(['xkblayout-state', 'set', '+1'])
+        # changerEx = changer.poll()
 
-        lytAfterProc = subprocess.Popen(['xkblayout-state', 'print', '"%n"'], stdout=subprocess.PIPE) #setxkbmap
-        lytAfter = lytAfterProc.communicate()[0].decode("utf-8") 
-        lytAfterProc.poll()
-        lytAfter = lytAfter[1:-1]
+        # var = ''
 
-        selectedText = subprocess.Popen(['xsel'], stdout=subprocess.PIPE) 
-        var = selectedText.communicate()[0].decode("utf-8") 
-        var = var
-        print(var)
-        stCode = selectedText.poll()
+        # lytAfterProc = subprocess.Popen(['xkblayout-state', 'print', '"%n"'], stdout=subprocess.PIPE) #setxkbmap
+        # lytAfter = lytAfterProc.communicate()[0].decode("utf-8") 
+        # lytAfterProc.poll()
+        # lytAfter = lytAfter[1:-1]
+
+        # selectedText = subprocess.Popen(['xsel'], stdout=subprocess.PIPE) 
+        # var = selectedText.communicate()[0].decode("utf-8") 
+        # var = var
+        # print(var)
+        # stCode = selectedText.poll()
 
 
-        rightDecode = ''                                     
-        print(lytBefor, 'Polish')
-        if lytBefor == 'Polish':
-            rightDecode = self.decodeIt(var, self.qwertyLat, self.qwertyCirr)
+        # rightDecode = ''                                     
+        # print(lytBefor, 'Polish')
+        # if lytBefor == 'Polish':
+        #     rightDecode = self.decodeIt(var, self.qwertyLat, self.qwertyCirr)
             
-        if lytBefor == 'Russian' or lytBefor == 'Ukrainian':
-            rightDecode = self.decodeIt(var, self.qwertyCirr, self.qwertyLat)
+        # if lytBefor == 'Russian' or lytBefor == 'Ukrainian':
+        #     rightDecode = self.decodeIt(var, self.qwertyCirr, self.qwertyLat)
 
-        ctrlV = subprocess.Popen(['xdotool', 'getactivewindow', 'windowfocus', 'type', rightDecode], stdout=subprocess.PIPE, \
-                                    stderr=subprocess.PIPE)  #'getactivewindow', 'windowfocus', 
-        ctrlV.wait()
-        ex = ctrlV.poll()
-        logging.info('------------------------------')
-        logging.info('Word before: \t\t' + var)
-        logging.info('Word after : \t\t' + rightDecode)
-        logging.info('xdotool exit code: ' + str(ex))
-        logging.info('xsel exit code: \t' + str(stCode))
-        logging.info('layout change exit code: ' + str(changerEx) + '\n')
-        lytBefor = lytAfter
+        # ctrlV = subprocess.Popen(['xdotool', 'getactivewindow', 'windowfocus', 'type', rightDecode], stdout=subprocess.PIPE, \
+        #                             stderr=subprocess.PIPE)  #'getactivewindow', 'windowfocus', 
+        # ctrlV.wait()
+        # ex = ctrlV.poll()
 
 if __name__ == "__main__" :
     subprocess.Popen(['killall', 'xclip'])
     rmpr = Remapper()    
     rmpr.main(sys.argv)
-    #rmpr.remapper()
     quit(0)
